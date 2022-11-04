25068f4715ec52af565fd59f2abf61e3f82bc99a
argjendR <gentxh@gmail.com>
2022-11-04 14:13:12 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
-    <div>
-      <div>className=</div>
+    <>
       <Navbar />
-    </div>
+      <Hero />
+      <About />
+    </>
   );
 }
 
