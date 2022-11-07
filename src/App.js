961bc9d16e9d95060eb03a913e137de9ba32f4c0
argjendR <gentxh@gmail.com>
2022-11-07 15:08:23 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Support from "./components/Support";
+import AllInOne from "./components/AllInOne";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
     </>
   );
 }
